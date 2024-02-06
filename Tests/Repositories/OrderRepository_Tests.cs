307a1d011e5acbd870a1be6f279b5961ecfa5a0d
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-02-06 17:01:51 +0100

    Tests for repositories: Order and Product is done.

--- Tests/Repositories/OrderRepository_Tests.cs
+++ Tests/Repositories/OrderRepository_Tests.cs
@@ -18,8 +18,15 @@ public class OrderRepository_Tests
     {
         // Arrange
         var orderRepository = new OrderRepository(_context);
-        var orderEntity = new OrderEntity { Status = "Status test"};
-
+        var userEntity = new UserEntity
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test.user@example.com"
+        };
+        _context.Users.Add(userEntity);
+        _context.SaveChanges();
+        var orderEntity = new OrderEntity { Status = "Status test", User = userEntity };
 
         // Act
         var result = orderRepository.Create(orderEntity);
@@ -27,6 +34,119 @@ public class OrderRepository_Tests
         // Assert
         Assert.NotNull(result);
         Assert.Equal(1, result.Id);
+
+    }
+
+    [Fact]
+    public void Get_ShouldGetAllOrders()
+    {
+        var orderRepository = new OrderRepository(_context);
+        var userEntity = new UserEntity
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test.user@example.com" 
+        };
+
+        _context.Users.Add(userEntity);
+        _context.SaveChanges();
+        var orderEntity = new OrderEntity { Status = "Status test", User = userEntity };
+        orderRepository.Create(orderEntity);
+
+        // Act
+        var result = orderRepository.GetAll();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<OrderEntity>>(result);
+        Assert.Single(result);
+
+
+    }
+
+    [Fact]
+    public void Get_ShouldGetOneOrder()
+    {
+        // Arrange
+        var orderRepository = new OrderRepository(_context);
+        var userEntity = new UserEntity
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test.user@example.com" 
+        };
+        _context.Users.Add(userEntity);
+        _context.SaveChanges();
+        var orderEntity = new OrderEntity { Status = "Status test", User = userEntity };
+        orderRepository.Create(orderEntity);
+
+        
+
+        // Act
+        var result = orderRepository.Get(x => x.Id == orderEntity.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(orderEntity.Id, result.Id);
+
+
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateOneOrder()
+    {
+        // Arrange
+        var orderRepository = new OrderRepository(_context);
+        var userEntity = new UserEntity
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test.user@example.com"
+        };
+
+        _context.Users.Add(userEntity);
+        _context.SaveChanges();
+        var orderEntity = new OrderEntity { Status = "Status test", User = userEntity };
+        orderRepository.Create(orderEntity);
+        
+
+        // Act
+        orderEntity.Status = "Updated status"; // Change a property
+        orderRepository.Update(x => x.Id == orderEntity.Id, orderEntity);
+        var result = orderRepository.Get(x => x.Id == orderEntity.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Updated status", result.Status);
+
+    }
+
+
+    [Fact]
+    public void Delete_ShouldDeleteOneOrder()
+    {
+        // Arrange
+        var orderRepository = new OrderRepository(_context);
+        var userEntity = new UserEntity
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test.user@example.com"
+        };
+
+        _context.Users.Add(userEntity);
+        _context.SaveChanges();
+        var orderEntity = new OrderEntity { Status = "Status test", User = userEntity };
+        orderRepository.Create(orderEntity);
+
+        // Act
+        orderRepository.Delete(x => x.Id == orderEntity.Id);
+        var result = orderRepository.Get(x => x.Id == orderEntity.Id);
+
+        // Assert
+        Assert.Null(result);
+
+
 
     }
 }
--- Tests/Repositories/ProductRepository_Tests.cs
+++ Tests/Repositories/ProductRepository_Tests.cs
@@ -28,4 +28,112 @@ public class ProductRepository_Tests
         Assert.Equal(1, result.Id);
 
     }
+
+    [Fact]
+    public void Get_ShouldGetAllProducts()
+    {
+        var productRepository = new ProductRepository(_context);
+        var productEntity = new ProductEntity
+        {
+            ProductName = "Test Product",
+            Price = 100.0m,
+            Description = "This is a test product",
+            CategoryId = 1,
+            Category = new CategoryEntity { CategoryName = "Test category" }
+        };
+
+        productRepository.Create(productEntity);
+
+        // Act
+        var result = productRepository.GetAll();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<ProductEntity>>(result);
+        Assert.Single(result);
+
+
+    }
+
+    [Fact]
+    public void Get_ShouldGetOneProduct()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var productEntity = new ProductEntity
+        {
+            ProductName = "Test Product",
+            Price = 100.0m,
+            Description = "This is a test product",
+            CategoryId = 1,
+            Category = new CategoryEntity { CategoryName = "Test category" }
+        };
+
+
+        productRepository.Create(productEntity);
+
+        // Act
+        var result = productRepository.Get(x => x.Id == productEntity.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(productEntity.Id, result.Id);
+
+
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateOneProduct()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var productEntity = new ProductEntity
+        {
+            ProductName = "Test Product",
+            Price = 100.0m,
+            Description = "This is a test product",
+            CategoryId = 1,
+            Category = new CategoryEntity { CategoryName = "Test category" }
+        };
+
+        productRepository.Create(productEntity);
+
+        // Act
+        productEntity.Price = 200.0m; // Change a property
+        productRepository.Update(x => x.Id == productEntity.Id, productEntity);
+        var result = productRepository.Get(x => x.Id == productEntity.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(200.0m, result.Price);
+
+    }
+
+
+    [Fact]
+    public void Delete_ShouldDeleteOneProduct()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var productEntity = new ProductEntity
+        {
+            ProductName = "Test Product",
+            Price = 100.0m,
+            Description = "This is a test product",
+            CategoryId = 1,
+            Category = new CategoryEntity { CategoryName = "Test category" }
+        };
+
+        productRepository.Create(productEntity);
+
+        // Act
+        productRepository.Delete(x => x.Id == productEntity.Id);
+        var result = productRepository.Get(x => x.Id == productEntity.Id);
+
+        // Assert
+        Assert.Null(result);
+
+
+
+    }
 }
